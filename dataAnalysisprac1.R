82720dd53468475c565bdf6c0d89abc68836d4bf
Arman Berek <armanberek@gmail.com>
2019-03-12 12:58:32 -0400

    added progress

--- dataAnalysisprac1.R
+++ dataAnalysisprac1.R
@@ -1,4 +1,7 @@
 head(dat)
-dat
-dat[12,2]
-dat$Bodyweight[11]
+dat #see data
+dat[12,2] #check cell on item 12 and column 2
+dat$Bodyweight[11] #extract eleventh item in Bodyweight column 
+length(dat$Diet) #number of items
+seq(3,7)
+sequ <- seq(3,7)
